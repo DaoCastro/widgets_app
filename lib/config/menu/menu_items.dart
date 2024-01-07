63dafd98cc17329eeab268125de120fdb6aed037
David Castro <dcastro@tribalworldwide.gt>
2024-01-06 18:45:46 -0600

    appthemes and menuitems

---
+++ lib/config/menu/menu_items.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class MenuItems {
+  final String title;
+  final String subTitle;
+  final String link;
+  final IconData icon;
+
+  const MenuItems({
+    required this.title,
+    required this.subTitle,
+    required this.link,
+    required this.icon,
+  });
+}
+
+const appMenuItem = <MenuItems>[
+  MenuItems(
+      title: 'Botones',
+      subTitle: 'Varios botones en flutter',
+      link: '/buttons',
+      icon: Icons.smart_button_outlined),
+  MenuItems(
+      title: 'Tarjetas',
+      subTitle: 'Un contenedor estilizado',
+      link: '/card',
+      icon: Icons.credit_card),
+];
