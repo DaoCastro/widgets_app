96bfab6ae735c23de7cd61ed12dffffe364f2549
David Castro <dcastro@tribalworldwide.gt>
2024-01-06 22:37:07 -0600

    creation other screen for app

---
+++ lib/presentation/screens/ui_controls/ui_controls_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class UIControlsScreen extends StatelessWidget {
+  static const name = "ui_controls_screen";
+  const UIControlsScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
